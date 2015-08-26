447e972ddaf35eec3b4093232c0aa7ecdaf8db03
Michael <avis-hermetis@yandex.ru>
2015-08-27 01:08:52 +1000

    Исправленная версия Задания 3

--- trains.rb
+++ trains.rb
@@ -1,60 +1,67 @@
 class Route
+  attr_reader :route
+  
   def initialize(from, to)
   @route = [from, to]  
   end
 
   def add(station)
-  @route[1, 0] = [station] #добавляет именно промежуточные станции
+  self.route.insert(1,station)  #исправил
   end
 
   def remove(station)
-    if @route.delete(station) == "not found" 
+    if self.route.delete(station) == "not found" 
       puts "Такой станции нет списке"
     end
   end
 
   def list
-   puts "Маршрут: #{@route.first} - #{@route.last}}" 
-   @route.each { |station|puts station}
-  end
+  puts "Маршрут: #{@route.first} - #{@route.last}" 
+    self.route.each { |station|puts station}
+    end
 
   def intermed_stations
-  puts @route[1...@route.size-1] #выводит промежуточные станции
+  puts self.route[1..-2] #исправил
   end
 end  
 
 
 class Train
-  def initialize(type = "passenger", route = ["Владивосток", "Москва"], wagons = 0)
+  attr_reader :train
+  attr_reader :route
+
+   def initialize(type = "passenger", route)
+    @speed = 0
     @type = type
     @route = route
-    @wagon = 0
-    @station_i = 0
+    @station_index = 0
     @current_point = @route[0]
     @next_point = @route[1] 
-    @previous_point
+    @previous_point 
+    @train = {type: @type, wagons: 0}
   end
 
-  def set_route(list)
-    @route = list
-    @station_i = 0
+    def set_route(route)  #не проверен до конца
+    @route = route
+    @station_index = 0
     @current_point = @route[0]
+    @next_point = @route[1] 
   end
 
   def add_wagon
-    @wagons += 1 if @speed.zero?
+    self.train[:wagons] += 1 if @speed.zero?
   end
 
   def remove_wagon
-    @wagons -= 1 if @speed.zero? && @wagons > 0
+    self.train[:wagons] -= 1 if @speed.zero? && self.train[:wagons] > 0
   end
 
   def wagons
-   puts "Количество вагонов #{@wagons}" 
+   puts "Количество вагонов #{self.train[:wagons]}" 
   end
 
   def speed
-    puts "Текущая скорость #{speed}"
+    puts "Текущая скорость #{@speed}" 
   end
 
   def accelerate
@@ -64,48 +71,55 @@ class Train
 
   def retard
     puts "Снижаем скорость на 40 км/ч"
-    @speed -= 40 
-    @speed = 0 if @speed <= 40
+    if @speed >= 40 
+      @speed -= 40 
+    else
+      @speed = 0
+    end
   end
 
   def departure
-    if @station_index < @route.size - 1
-      puts "Поезд отправляется со станции #{@route[@station_index]}, следующая станция #{@route[@station_index + 1]}"
+    if @station_index < @route.size - 1     
+      puts "Поезд отправляется со станции #{self.route[@station_index]}, следующая станция #{self.route[@station_index + 1]}"
       @speed += 20
     else
-      puts "Поезд прибыл на конечную станцию #{@route[@station_index]}"
+      puts "Поезд прибыл на конечную станцию #{self.route[@station_index]}"
     end
   end
 
   def arrive
-   puts "Поезд прибыл на станцию #{@route[@station_index + 1]}" 
+   puts "Поезд прибыл на станцию #{self.route[@station_index + 1]}" 
    @speed = 0  
-   @station_index += 1 until @station_index = @route.size - 1
+   @station_index += 1 if @station_index < @route.size - 1
   end
 
-  def notice
-    case @speed 
+  def notice #неполная инфа
+        case @speed 
       when 0
         puts "Поезд стоит на станции #{@route[@station_index]}"
       else
         puts "Поезд движется со скоростью #{@speed}"
-    end
+       end
     
     puts "Предыдущая станция #{@route[@station_index - 1]}" if @station_index > 0
-    puts "Следующая станция #{@route[@station_index + 1]}" until @station_index = @route.size - 1
+    puts "Следующая станция #{@route[@station_index + 1]}" if @station_index < @route.size - 1
   end
   
 end  
 
 class RailwayStation
+  attr_reader :train
+
   def initialize(name)
     @name = name
     @train_list = Array.new
     end  
 
-  def accept(type, number_of_wagons)
-    @train_list << {type: type, wagons: number_of_wagons, time_arrival: Time.new}
-  end
+  def accept(train)
+    self.train = train
+    self.train[:time_arrival] = Time.now  
+    @train_list << self.train 
+  end 
 
   def send
     @train_list.shift
